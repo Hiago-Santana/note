660e23a3c89eb63fe2bbfac2b9fe23d723f80c26
Hiago-Santana <hiago_douglas@hotmail.com>
2023-11-23 09:25:42 -0300

    update

--- src/components/Worker.js
+++ src/components/Worker.js
@@ -1,4 +1,3 @@
-//////////const api_url =	"https://frosty-recipe-fd6f.hiago-douglas.workers.dev";
 //const api_url = "http://127.0.0.1:8787/";
 const api_url =	"https://worker1note.hiago-douglas.workers.dev"
 
